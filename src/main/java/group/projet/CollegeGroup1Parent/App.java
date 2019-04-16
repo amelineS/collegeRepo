f08955c4a3c6b1d9982962d1e7da5d47148e63dc
amelineS <sehan.ameline@gmail.com>
2019-04-16 17:33:48 +0200

    first commit

---
+++ src/main/java/group/projet/CollegeGroup1Parent/App.java
@@ -0,0 +1,13 @@
+package group.projet.CollegeGroup1Parent;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
