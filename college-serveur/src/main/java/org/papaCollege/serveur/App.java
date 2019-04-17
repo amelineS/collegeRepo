e45d1550c5bcbc378a8afa67307a0c03a5dfc800
IN-NN-010 <IN-NN-010@192.168.1.116>
2019-04-17 11:11:11 +0200

    premier livrable

---
+++ college-serveur/src/main/java/org/papaCollege/serveur/App.java
@@ -0,0 +1,25 @@
+package org.papaCollege.serveur;
+
+import org.papaCollege.service.IDepartementMetier;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+       
+    	ApplicationContext ctx = new ClassPathXmlApplicationContext("config.xml");
+    	
+    	IDepartementMetier serviceD = (IDepartementMetier) ctx.getBean("serviceDepartement");
+    	
+    	
+    	
+    	
+    	
+    }
+}
