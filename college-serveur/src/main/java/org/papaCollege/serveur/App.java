8608f893b2b8e758374ecc277646c0141cc29264
IN-NN-001 <IN-NN-001@192.168.1.198>
2019-04-17 15:33:48 +0200

    commit test

--- college-serveur/src/main/java/org/papaCollege/serveur/App.java
+++ college-serveur/src/main/java/org/papaCollege/serveur/App.java
@@ -1,37 +1,10 @@
 package org.papaCollege.serveur;
 
 
-<<<<<<< HEAD
-import org.papaCollege.entities.Noter;
-import org.papaCollege.service.INoterMetier;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-=======
->>>>>>> branch 'master' of https://github.com/amelineS/collegeRepo.git
-
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
     {
-<<<<<<< HEAD
-       
-    	ApplicationContext ctx = new ClassPathXmlApplicationContext("config.xml");
-    	
-    	INoterMetier serviceN = (INoterMetier) ctx.getBean("serviceNoter");
-    	
-    	
-    	Noter n1 = new Noter(12, null, null);
-    	serviceN.ajouter(n1);
-    	
-    	//System.out.println(serviceN.afficher());
-=======
- 
-    
->>>>>>> branch 'master' of https://github.com/amelineS/collegeRepo.git
-    	
+
     }
 }
