985065629d89c0011d1ac47b35db279d610e03c4
amelineS <sehan.ameline@gmail.com>
2019-04-17 15:20:16 +0200

    commit app

--- college-serveur/src/main/java/org/papaCollege/serveur/App.java
+++ college-serveur/src/main/java/org/papaCollege/serveur/App.java
@@ -1,13 +1,7 @@
 package org.papaCollege.serveur;
 
 
-<<<<<<< HEAD
-import org.papaCollege.entities.Noter;
-import org.papaCollege.service.INoterMetier;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-=======
->>>>>>> branch 'master' of https://github.com/amelineS/collegeRepo.git
+
 
 /**
  * Hello world!
@@ -17,21 +11,7 @@ public class App
 {
     public static void main( String[] args )
     {
-<<<<<<< HEAD
-       
-    	ApplicationContext ctx = new ClassPathXmlApplicationContext("config.xml");
-    	
-    	INoterMetier serviceN = (INoterMetier) ctx.getBean("serviceNoter");
-    	
-    	
-    	Noter n1 = new Noter(12, null, null);
-    	serviceN.ajouter(n1);
-    	
-    	//System.out.println(serviceN.afficher());
-=======
- 
-    
->>>>>>> branch 'master' of https://github.com/amelineS/collegeRepo.git
+
     	
     }
 }
