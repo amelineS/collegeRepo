d12b3cfe6288a6ff239d35f21715039d5fcf85dd
IN-NN-010 <IN-NN-010@192.168.1.116>
2019-04-17 12:10:02 +0200

    commit controller

---
+++ college-web/src/main/java/org/papaCollege/controller/testController.java
@@ -0,0 +1,45 @@
+package org.papaCollege.controller;
+
+import org.papaCollege.entities.Colleges;
+import org.papaCollege.entities.Enseignant;
+import org.papaCollege.entities.Matiere;
+import org.papaCollege.entities.Salle;
+import org.papaCollege.service.IDepartementMetier;
+import org.papaCollege.service.IEtudiantMetier;
+import org.papaCollege.service.IGestionCollegeMetier;
+import org.papaCollege.service.INoterMetier;
+import org.papaCollege.service.IPersonneMetier;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class testController {
+
+	@Autowired
+	private IDepartementMetier serviceDepartement;
+	@Autowired
+	private IEtudiantMetier serviceEtudiant;
+	@Autowired
+	private INoterMetier serviceNoter;
+	@Autowired
+	private IPersonneMetier servicePersonne;
+	@Autowired
+	private IGestionCollegeMetier<Colleges> serviceCollege;
+	@Autowired
+	private IGestionCollegeMetier<Enseignant> serviceEnseignant;
+	
+	@Autowired
+	private IGestionCollegeMetier<Matiere> serviceMatiere;
+	@Autowired
+	private IGestionCollegeMetier<Salle> serviceSalle;
+	
+	@RequestMapping(value="/admin/home")
+	public String homeAdmin() {
+	
+	return "Home";
+	}
+
+
+	
+}
