1b0ea18d0f9854a400ce48cc5b82eff9513bc349
IN-NN-001 <IN-NN-001@192.168.1.198>
2019-04-17 15:11:51 +0200

    commit nico

--- college-serveur/src/main/java/org/papaCollege/serveur/App.java
+++ college-serveur/src/main/java/org/papaCollege/serveur/App.java
@@ -1,6 +1,8 @@
 package org.papaCollege.serveur;
 
-import org.papaCollege.service.IDepartementMetier;
+
+import org.papaCollege.entities.Noter;
+import org.papaCollege.service.INoterMetier;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -15,11 +17,13 @@ public class App
        
     	ApplicationContext ctx = new ClassPathXmlApplicationContext("config.xml");
     	
-    	IDepartementMetier serviceD = (IDepartementMetier) ctx.getBean("serviceDepartement");
+    	INoterMetier serviceN = (INoterMetier) ctx.getBean("serviceNoter");
     	
     	
+    	Noter n1 = new Noter(12, null, null);
+    	serviceN.ajouter(n1);
     	
-    	
+    	//System.out.println(serviceN.afficher());
     	
     }
 }
